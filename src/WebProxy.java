3f3b8230f3d49a0be576dcdfd35a0cac0bd9479e
Israel Hill <idh@case.edu>
2015-10-24 15:51:56 -0400

    added threading

--- src/WebProxy.java
+++ src/WebProxy.java
@@ -1,89 +1,29 @@
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.net.ServerSocket;
 
 public class WebProxy {
-    public static void main(String[] args) {
-        System.out.println("Listening on port: " +  args[0]);
-        int portNumber = Integer.valueOf(args[0]);
-        run(portNumber);
-    }
 
-    public static void run(int port) {
+    public static void main(String[] args) {
+        ServerSocket connection = null;
+        final int PORT = Integer.valueOf(args[0]);
+        System.out.println("Listening on port " + PORT);
 
         try {
-            ServerSocket connection = new ServerSocket(port);
-            byte[] req = new byte[1024];
-            byte[] reply = new byte[4096];
-
-
-            while(true) {
-                Socket client;
-                Socket server;
-
-                // accept an incoming request from the browser
-                client = connection.accept();
-
-                // initialize an input and output stream for the client
-                final InputStream clientIn = client.getInputStream();
-                final OutputStream clientOut = client.getOutputStream();
-
-
-                StringBuilder builder = new StringBuilder();
-                int len = clientIn.read(req);
-                if(len > 0) {
-
-                    String str;
-                    str = new String(req, 0, len);
-
-                    final int HTTP_LENGTH = 7;
-                    System.out.println("REQUEST: " + "\n" + str + "\n");
-                    String replace = str.replace("\n", " ");
-                    String[] request = replace.split(" ");
-
-                    System.out.println("URL: " + request[1]);
-                    String hostName = request[4].trim();
-                    System.out.println("Host name: " + hostName);
-
-
-                    server = new Socket(hostName, 80);
-                    String dir = request[1].substring(HTTP_LENGTH + hostName.length());
-                    System.out.println("Directory: " + dir);
-
-                    PrintWriter serv = new PrintWriter(new BufferedWriter(new OutputStreamWriter(server.getOutputStream())));
-                    System.out.println("Sending get request to: " + request[1]);
-                    serv.println("GET " + dir + " HTTP/1.1");
-                    for(int i = 0; i < req.length; i++) {
-
-                        serv.println(req[i]);
-                    }
-                    serv.println();
-                    serv.flush();
-
-                    //write request
-                    OutputStream serverOut = server.getOutputStream();
-                    serverOut.write(req, 0, len);
-
-                    //copy response
-                    InputStream outGoingStream = server.getInputStream();
-                    for(int length; (length = outGoingStream.read(req)) != -1;) {
-                        clientOut.write(req, 0, length);
-                    }
-
-                    clientIn.close();
-                    clientOut.close();
-                    serverOut.close();
-                    outGoingStream.close();
-
-                    server.close();
-
-                }
-                else {
-                    clientOut.close();
-                }
-            }
+            connection = new ServerSocket(PORT);
+            System.out.println("Listening for requests from browser");
         }
-        catch (IOException e) {
-            e.printStackTrace();
+        catch(IOException e) {
+            System.out.println("An error occurred.");
+            // Program failed. Exit
+            System.exit(-1);
+        }
+
+        while(true) {
+            try {
+                new WebProxyThread(connection.accept()).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }

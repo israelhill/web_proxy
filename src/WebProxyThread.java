f694856c1513dab84b206446a5b397a8b2b12d57
Israel Hill <idh@case.edu>
2015-10-28 19:24:51 -0400

    proxy in working state. need to look into relative/absolute urls

--- src/WebProxyThread.java
+++ src/WebProxyThread.java
@@ -12,6 +12,8 @@ public class WebProxyThread extends Thread {
     @Override
     public void run() {
         Socket server = null;
+        final int BUFFER_SIZE = 2048;
+        final int HTTP_LENGTH = 7;
 
         try {
             byte[] req = new byte[2048];
@@ -20,59 +22,86 @@ public class WebProxyThread extends Thread {
             final InputStream clientIn = connectionSocket.getInputStream();
             final OutputStream clientOut = connectionSocket.getOutputStream();
 
-            // read the HTTP request from the client. Store in req array
-            int requestLength = clientIn.read(req);
-            if(requestLength > 0) {
-                //Store the HTTP Request as a string
-                String str = new String(req, 0, requestLength);
+            StringBuilder builder = new StringBuilder();
+            InputStreamReader reader = new InputStreamReader(clientIn);
+            BufferedReader br = new BufferedReader(reader);
+            String msg;
+            String close = "Connection: close";
+            String directory = "";
+            String host = "";
 
-                final int HTTP_LENGTH = 7;
-                String replace = str.replace("\n", " ");
-                //Split the request at spaces
-                String[] request = replace.split(" ");
-                String hostName = request[4].trim();
-
-                //Create a new socket to talk to the host
-                server = new Socket(hostName, 80);
-                //get the directory of the content
-                String dir = request[1].substring(HTTP_LENGTH + hostName.length());
-                System.out.println("Directory: " + dir);
-
-                //write request to the host via server socket
-                OutputStream serverOut = server.getOutputStream();
-                serverOut.write(req, 0, requestLength);
-                String requestToHost = new String(req, 0, requestLength);
-                serverOut.flush();
-                System.out.println("REQUEST TO HOST: " + "\n" + requestToHost);
-
-                //copy response from the host via server socket
-                InputStream responseStream = server.getInputStream();
-                int length;
-
-                // read HTTP response from the server socket and write it to the
-                // client socket so it can be passed back to the browser
-                try {
-                    while((length = responseStream.read(req)) != -1) {
-                        clientOut.write(req, 0, length);
-                        clientOut.flush();
-                    }
+            while((msg = br.readLine()) != null && !msg.isEmpty()) {
+                if(msg.contains("keep-alive")) {
+                    builder.append(close);
                 }
-                catch(IOException e) {
-
+                else if(msg.contains("Host:")) {
+                    String[] split = msg.split(" ");
+                    host = split[1];
+                    builder.append(msg);
                 }
+                else {
+                    builder.append(msg);
+                }
+                builder.append("\r\n");
+            }
+            builder.append("\r\n");
 
-                // close sockets and streams
-                clientIn.close();
-                clientOut.close();
-                serverOut.close();
-                responseStream.close();
+            System.out.println("HOST: " + host);
+
+            String strRequest = builder.toString();
+            byte[] arr = strRequest.getBytes();
+            int length = arr.length;
+//            System.out.println("SIZE: " + length);
+//
+//            System.out.println("REQUEST: " + "\n");
+//            System.out.println(strRequest);
+
+            server = new Socket(host, 80);
+
+            //write request to the host via server socket
+            OutputStream serverOut = server.getOutputStream();
+            serverOut.write(arr);
+
+            String requestToHost = new String(arr);
+            serverOut.flush();
+            System.out.println("REQUEST TO HOST: " + "\n" + requestToHost);
+
+            //copy response from the host via server socket
+            InputStream responseStream = server.getInputStream();
+
+            int res = responseStream.read();
+            try {
+                while(res != -1) {
+                    clientOut.write(res);
+                    clientOut.flush();
+                    res = responseStream.read();
+                }
             }
-            else {
-                clientOut.close();
+            catch(IOException e) {
+                System.out.println("ERROR:" + "\n");
+                e.printStackTrace();
             }
+
+            // close connections
+            serverOut.close();
+            responseStream.close();
+            clientOut.close();
+            clientIn.close();
+
+            server.close();
         }
         catch (IOException e) {
-            //e.printStackTrace();
+            System.out.println("ERROR:" + "\n");
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                System.out.println("Closing connection socket.");
+                connectionSocket.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
